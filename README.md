9425659d7e24d05476b00007a7c2337d73f335d7
Hzzone <zhizhonghwang@gmail.com>
2018-05-18 19:59:49 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Human Detection and Action Analysis
+Based on SSD and [Intel® Movidius™ Neural Compute Stick](https://developer.movidius.com/)
+
+
+
+## demo
+#### Detection
+![](pic/detection_demo.png)
---
+++ ssd_detection.py
@@ -0,0 +1,100 @@
+# Make sure that caffe is on the python path:
+caffe_root = 'caffe'  # this file is expected to be in {caffe_root}/examples
+import os
+os.chdir(caffe_root)
+import sys
+sys.path.insert(0, 'python')
+
+import caffe
+# caffe.set_device(0)
+caffe.set_mode_cpu()
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams['figure.figsize'] = (10, 10)
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+
+from google.protobuf import text_format
+from caffe.proto import caffe_pb2
+
+model_def = '/Users/hzzone/Desktop/SSD/models/deploy.prototxt'
+model_weights = '/Users/hzzone/Desktop/SSD/models/VGG_coco_SSD_300x300_iter_400000.caffemodel'
+
+net = caffe.Net(model_def,      # defines the structure of the model
+                model_weights,  # contains the trained weights
+                caffe.TEST)     # use test mode (e.g., don't perform dropout)
+
+# input preprocessing: 'data' is the name of the input blob == net.inputs[0]
+transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+transformer.set_transpose('data', (2, 0, 1))
+transformer.set_mean('data', np.array([104,117,123])) # mean pixel
+transformer.set_raw_scale('data', 255)  # the reference model operates on images in [0,255] range instead of [0,1]
+transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB
+
+# set net to batch size of 1
+image_resize = 300
+net.blobs['data'].reshape(1,3,image_resize,image_resize)
+
+image = caffe.io.load_image('/Users/hzzone/Desktop/SSD/example.jpg')
+# plt.imshow(image)
+# plt.show()
+plt.subplot(121)
+plt.axis('off')
+plt.imshow(image)
+
+
+
+transformed_image = transformer.preprocess('data', image)
+net.blobs['data'].data[...] = transformed_image
+
+# Forward pass.
+detections = net.forward()['detection_out']
+
+# Parse the outputs.
+det_label = detections[0,0,:,1]
+det_conf = detections[0,0,:,2]
+det_xmin = detections[0,0,:,3]
+det_ymin = detections[0,0,:,4]
+det_xmax = detections[0,0,:,5]
+det_ymax = detections[0,0,:,6]
+
+# Get detections with confidence higher than 0.6.
+top_indices = [i for i, conf in enumerate(det_conf) if conf >= 0.6]
+
+top_conf = det_conf[top_indices]
+top_label_indices = det_label[top_indices].tolist()
+# top_labels = get_labelname(labelmap, top_label_indices)
+top_xmin = det_xmin[top_indices]
+top_ymin = det_ymin[top_indices]
+top_xmax = det_xmax[top_indices]
+top_ymax = det_ymax[top_indices]
+
+
+
+colors = plt.cm.hsv(np.linspace(0, 1, 21)).tolist()
+
+plt.subplot(122)
+plt.axis('off')
+plt.imshow(image)
+
+currentAxis = plt.gca()
+
+for i in xrange(top_conf.shape[0]):
+    xmin = int(round(top_xmin[i] * image.shape[1]))
+    ymin = int(round(top_ymin[i] * image.shape[0]))
+    xmax = int(round(top_xmax[i] * image.shape[1]))
+    ymax = int(round(top_ymax[i] * image.shape[0]))
+    score = top_conf[i]
+    label = int(top_label_indices[i])
+    # label_name = top_labels[i]
+    # display_txt = '%s: %.2f'%(label_name, score)
+    display_txt = '%s: %.2f'%('person', score)
+    coords = (xmin, ymin), xmax-xmin+1, ymax-ymin+1
+    color = colors[label]
+    currentAxis.add_patch(plt.Rectangle(*coords, fill=False, edgecolor=color, linewidth=2))
+    currentAxis.text(xmin, ymin, display_txt, bbox={'facecolor':color, 'alpha':0.5})
+
+plt.show()
